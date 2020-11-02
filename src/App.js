7bcafa04d74976636c851e2fd17a4db497d17967
Vince <53957319+vincengai@users.noreply.github.com>
2020-11-02 11:21:37 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const API_URL = 'https://opentdb.com/api.php?amount=10&category=32&difficulty=medium';
+
+function App() {
+    return (
+      <div className="container">
+            <div> this is where the Qeuestinos gonna be</div>
+
+            <div>
+              <div>Answer 1</div>
+              <div>Answer 2</div>
+              <div>Answer 3</div>
+              <div>Answer 4</div>
+            </div>
+      </div>
+    )
+}
+
+export default App;
