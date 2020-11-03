5fd402059edf9a993e698d0136dad5fc6ff92830
Vince <53957319+vincengai@users.noreply.github.com>
2020-11-03 13:36:06 -0800

    updating rendering

--- src/App.js
+++ src/App.js
@@ -5,22 +5,28 @@ const API_URL = 'https://opentdb.com/api.php?amount=10&category=32&difficulty=me
 
 function App() {
   const [questions, setQuestions] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(undefined)
 
   useEffect( () => {
     fetch(API_URL)
       .then(res => res.json())
       .then((data) => {
         setQuestions(data.results)
+        setCurrentQuestion(data.results[0])
       });
 
   }, [])  
 
   const handleAnswer = (answer) => {
     // etc
+
+    // show another question
+    // change score if correct
+    // 
   }
     return questions.length > 0 ? (
       <div className="container">
-        <Questionaire data={questions[0]} handleAnswer={handleAnswer}/>
+        <Questionaire data={currentQuestion} handleAnswer={handleAnswer}/>
       </div>
     ) : (
       <h2 className="text-2xl text-white font-bold">Loading..</h2>
--- src/components/questionaire.js
+++ src/components/questionaire.js
@@ -1,11 +1,5 @@
 import React from 'react';
 
-
-const Button = ({ answer }) => (
-    <button className='bg-white p-4 text-purple-800 font-semibold rounded shadow'>{answer}</button>
-)
-
-
 const Questionaire = ({handleAnswer, data: {question, correct_answer, incorrect_answers}}) => {
     const shuffledAnswers = [correct_answer, ...incorrect_answers].sort( () => Math.random() - 0.5);
 
@@ -17,10 +11,11 @@ const Questionaire = ({handleAnswer, data: {question, correct_answer, incorrect_
             </div>
 
             <div className='grid grid-cols-2 gap-6 mt-6'>
-                <Button className={correct_answer === shuffledAnswers[0] ? 'bg-purple-300' : ''} onClick={() => handleAnswer(shuffledAnswers[0])} answer={shuffledAnswers[0]}/>
-                <Button className={correct_answer === shuffledAnswers[1] ? 'bg-purple-300' : ''} onClick={() => handleAnswer(shuffledAnswers[1])} answer={shuffledAnswers[1]}/>
-                <Button className={correct_answer === shuffledAnswers[2] ? 'bg-purple-300' : ''} onClick={() => handleAnswer(shuffledAnswers[2])} answer={shuffledAnswers[2]}/>
-                <Button className={correct_answer === shuffledAnswers[3] ? 'bg-purple-300' : ''} onClick={() => handleAnswer(shuffledAnswers[3])} answer={shuffledAnswers[3]}/>
+                {shuffledAnswers.map( (answer) => (
+                    <button className={`${correct_answer === answer ? 'bg-purple-300' : 'bg-white'} p-4 text-purple-800 font-semibold rounded shadow`}
+                        onClick={() => handleAnswer(answer)}> {answer} 
+                    </button>  
+                ))}
             </div>
         </div>
     );
